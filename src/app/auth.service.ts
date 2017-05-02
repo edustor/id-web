82e8b59d492fe5f62d31cd6f98f48fe01316dfc9
Dmitry Romanov <me@wutiarn.ru>
2017-05-02 17:21:50 +0300

    Get AuthToken from accounts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,19 +1,38 @@
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
 import {Injectable} from "@angular/core";
 import {Logger} from "angular2-logger/core";
+import {Http} from "@angular/http";
+import "rxjs/Rx";
 
 @Injectable()
 export class AuthService {
 
-  accessToken: String = null;
+  accessToken: AuthToken = null;
 
-  constructor(private router: Router, private logger: Logger) {
+  constructor(private logger: Logger, private http: Http) {
   }
 
   async login(googleIdToken: String): Promise<boolean> {
     this.logger.info(`Logging in with Google ID Token ${googleIdToken}`);
-    this.accessToken = "temp";
+
+    let formData = new FormData();
+
+    formData.append("grant_type", "password");
+    formData.append("username", "@google");
+    formData.append("password", googleIdToken);
+    formData.append("scope", "interactive token-issue");
+
+    let token = await this.http.post("http://localhost:8081/oauth2/token", formData)
+      .map(response => response.json() as AuthToken)
+      .toPromise();
+
+    this.accessToken = token;
 
     return true
   }
+}
+
+class AuthToken {
+  token: String;
+  expires_in: Number;
+  scope: String;
 }
