77b0f0b713eb6bff6ab5b05bea6e2dac0b0562f2
Dmitry Romanov <me@wutiarn.ru>
2017-05-02 14:33:40 +0300

    Fix google sign in button

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,14 +13,40 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    (<any> window).onGoogleSignIn = (googleUser: any) => {
-      let idToken = googleUser.getAuthResponse().id_token;
-      let profile = googleUser.getBasicProfile();
-      let email = profile.getEmail();
-      let name = profile.getName();
-      this.logger.info(`Google sign in finished: ${name} (${email})`);
-
-      this.authService.login(idToken)
+    this.logger.info(document.readyState);
+    if ((<any>window).gapi != undefined) {
+      this.setupGoogleSignInButton()
     }
+
+    (<any> window).onGoogleApiLoaded = () => {
+      this.setupGoogleSignInButton()
+    }
+  }
+
+  setupGoogleSignInButton() {
+    this.logger.info(`Setting up google sign in button...`);
+    gapi.load("auth2", () => {
+      gapi.auth2.init({
+        client_id: "99685742253-41uieqd0vl3e03l62c7t3impd38gdt4q.apps.googleusercontent.com"
+      });
+
+      gapi.signin2.render("google-sign-in-btn", {
+        onsuccess: (googleUser: any) => {
+          this.onGoogleSignedIn(googleUser)
+        }
+      });
+
+      this.logger.info(`Setting up google sign in button done`);
+    });
+  }
+
+  onGoogleSignedIn(googleUser: any) {
+    let idToken = googleUser.getAuthResponse().id_token;
+    let profile = googleUser.getBasicProfile();
+    let email = profile.getEmail();
+    let name = profile.getName();
+    this.logger.info(`Signed in with Google as ${name} (${email})`);
+
+    this.authService.login(idToken)
   }
 }
